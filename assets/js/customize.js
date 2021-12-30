844c1b14603e62ed032199a6e44ea0a6dd4407ca
panyulive <f2b239@gmail.com>
2021-12-30 12:45:31 +0900

    テーブル生成追加

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -10,18 +10,24 @@ function viewPrintTone(tone,unshi){
   unshiArray = unshi.split(/\n/).map(function(row){
     return row.split(',');
   });
-  unshiArray.shift();
-  //console.log(toneArray);
+
+  var head = unshiArray.shift();
+  var table = [];
 
   var key = DEFAULT_KEY + parseInt(SELECT);
+
   for (let step = 0; step < unshiArray.length; step++){
-    
-    console.log(unshiArray[step]);
-    console.log(toneArray[key]);
+    //console.log(unshiArray[step]); //運指表より順番に出力
+    //console.log(toneArray[key]);   //選択されたキーより順番に出力
+
+    table[step] = toneArray[key];
     key = parseInt(key) + parseInt(unshiArray[step][2]);
-    
   }
 
+  var hash = createHashFromAry(head,table);
+  var html = createTbl_hash(hash);
+
+  $("#table").html(html);
   return null;
 }
 
@@ -45,11 +51,66 @@ $(function(){
           $.ajax(unshi),
         ).done(function(res1,res2){
           viewPrintTone(res1[0],res2[0]);
-          //console.log(res1[0]);
-          //console.log(res2[0]);
         }).fail(function(){
           alert("エラーが発生しました。しばらくお待ち下さい");
         });
       });
     });
   });
+
+  /**
+ * 2次元配列とキー配列から連想配列オブジェクト（エンティティリスト）を作成する。
+ * @param keys キー配列
+ * @param ary  2次元配列
+ * @returns 連想配列オブジェクト
+ */
+function createHashFromAry(keys,ary){
+	var obj={};
+	var x_cnt=ary[0].length;
+	for(var y=0;y<ary.length;y++){
+		var ent={};
+		for(var x=0;x<x_cnt;x++){
+
+			ent[keys[x]]=ary[y][x];
+		}
+		obj[y]=ent;
+	}
+
+	return obj;
+}
+
+
+/**
+ * 連想配列オブジェクトからテーブルHTMLを生成する。
+ * キーをヘッダーの名前に利用する。
+ * @param hash 連想配列オブジェクト
+ * @return テーブルHTML
+ */
+function createTbl_hash(hash){
+
+
+	//1行目のエンティティからヘッダー部分を組み立て
+	var html="<table border='1'><thead><tr>"
+	for(var k in hash[0]){
+		html+="<th>" + k + "</th>";
+	}
+	html+="</th></thead>\n";
+	html+="<tbody>\n";
+
+
+
+	//連想配列をループ。
+	for(var i in hash){
+		html+="<tr>";
+		var ent=hash[i];
+		for(var k in ent){
+			var v=ent[k];
+			html+="<td>" + v + "</td>";
+		}
+		html+="</tr>\n";
+	}
+
+	html+="</tbody></table>\n";
+
+	return html;
+}
